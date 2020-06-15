c8a0fe8de252f4eb5a6fa11e50f83df49fe45d56
Hamza <hamza.husein017@gmail.com>
2020-06-15 20:56:27 +0530

    add button click press animation, set the delete and clearall button, add attribution

--- script.js
+++ script.js
@@ -2,40 +2,58 @@ let outputScreen = document.getElementById("screen");
 let calculatorButtons = document.querySelectorAll("button");
 let outputScreenValue = "";
 //console.log(calculatorButtons)
+let lastPress= ""
 
 for(button of calculatorButtons){
     //console.log(buttonValue)
+    
     button.addEventListener("click", function(event){
         buttonValue = event.target.innerText;
         //console.log(buttonNumber);
+
+    
     
         if (buttonValue === "AC"){
-        outputScreenValue = "";
-        outputScreen.value = outputScreenValue;
+            outputScreenValue = "";
+            outputScreen.value = outputScreenValue
+        }
+        else if (buttonValue === "C"){
+            outputScreenValue = outputScreenValue.slice(0, outputScreen.value.length -1);
+            outputScreen.value = outputScreenValue
         }
         else if(buttonValue === "="){
             outputScreen.value = eval(outputScreenValue);
+            lastPress = outputScreen.value
         }
         else{
-            outputScreenValue += buttonValue;
+            outputScreenValue += buttonValue;    
             //console.log(outputScreenValue);
             outputScreen.value = outputScreenValue;
+            
+            
         }
         
     });
+
+    
     
 }
 
 document.addEventListener("keydown", function(event){
     buttonValue = event.key;
-    //console.log(buttonNumber);
+    //console.log(buttonValue);
 
     if (buttonValue === "Delete"){
         outputScreenValue = "";
         outputScreen.value = outputScreenValue;
     }
+    else if (buttonValue === "Backspace"){
+        outputScreenValue = outputScreenValue.slice(0, outputScreen.value.length -1);
+        outputScreen.value = outputScreenValue
+    }
     else if(buttonValue === "=" || buttonValue === "Enter" ){
         outputScreen.value = eval(outputScreenValue);
+        lastPress = outputScreen.value
     }
     else if(buttonValue === "/"){
         outputScreenValue += "/";
@@ -55,7 +73,11 @@ document.addEventListener("keydown", function(event){
     }
     else if (!isNaN(buttonValue)){
         outputScreenValue += parseInt(buttonValue);
-        console.log(outputScreenValue);
+        //console.log(outputScreenValue);
         outputScreen.value = outputScreenValue;
+    }
+
+    if(lastPress === outputScreen.value){
+        outputScreenValue="";
     }
 });
